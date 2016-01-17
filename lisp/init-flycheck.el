fac6a1f734b77ccbbb64c27f7842b77c34cf1b05
Choi Geonu <6566gun@gmail.com>
2016-01-17 19:35:27 +0900

    Add new-line for flycheck syntax check

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -2,7 +2,7 @@
   (add-hook 'after-init-hook 'global-flycheck-mode)
 
   ;; Override default flycheck triggers
-  (setq flycheck-check-syntax-automatically '(save idle-change mode-enabled)
+  (setq flycheck-check-syntax-automatically '(save idle-change mode-enabled new-line)
         flycheck-idle-change-delay 0.5)
 
   (setq flycheck-display-errors-function #'flycheck-display-error-messages-unless-error-list)
