a0600e71b563f258ecff5c6e0d5d9a66827d530c
Choi Geonu <6566gun@gmail.com>
2016-05-04 14:55:55 +0900

    Enable hl-line-mode and change cursor-type

--- lisp/init-appearance.el
+++ lisp/init-appearance.el
@@ -1,3 +1,10 @@
+;; Display time to mode-line
 (display-time-mode 1)
+
+;; Hightlight current line
+(global-hl-line-mode 1)
+
+;; Bar-cursor
+(setq-default cursor-type 'bar)
 
 (provide 'init-appearance)
