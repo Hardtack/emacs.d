f63e2bc9c93fac13ba476f548cd816767e0fde69
Choi Geonu <6566gun@gmail.com>
2016-06-07 11:49:22 +0900

    Clojure with paredit

--- lisp/init-clojure.el
+++ lisp/init-clojure.el
@@ -4,9 +4,11 @@
 (require-package 'cljsbuild-mode)
 (require-package 'elein)
 
-(after-load 'clojure-mode
-  (add-hook 'clojure-mode-hook 'sanityinc/lisp-setup)
-  (add-hook 'clojure-mode-hook 'subword-mode))
+(require 'clojure-mode)
+
+(add-hook 'clojure-mode-hook 'paredit-mode)
+(add-hook 'clojure-mode-hook 'sanityinc/lisp-setup)
+(add-hook 'clojure-mode-hook 'subword-mode)
 
 
 (provide 'init-clojure)
