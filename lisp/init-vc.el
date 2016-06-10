d066f38f49d7788fa45f0844f1b57b928c78169c
Choi Geonu <6566gun@gmail.com>
2016-06-10 17:51:02 +0900

    Highlight diff on the fly

--- lisp/init-vc.el
+++ lisp/init-vc.el
@@ -1,6 +1,13 @@
 (require-package 'diff-hl)
-(add-hook 'prog-mode-hook 'turn-on-diff-hl-mode)
-(add-hook 'vc-dir-mode-hook 'turn-on-diff-hl-mode)
+(require 'diff-hl)
+(require 'diff-hl-flydiff)
+
+(defun turn-on-diff-hl-flydiff-mode ()
+  "Turn on diff-hl-flydiff-mode."
+  (interactive)
+  (diff-hl-flydiff-mode t))
+(add-hook 'prog-mode-hook 'turn-on-diff-hl-flydiff-mode)
+(add-hook 'vc-dir-mode-hook 'turn-on-diff-hl-flydiff-mode)
 
 
 (provide 'init-vc)
