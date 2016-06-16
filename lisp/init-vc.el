c9111cedd271d4abd34bdf426e54db9377c5ef86
Choi Geonu <6566gun@gmail.com>
2016-06-16 16:12:14 +0900

    no diff-hl-mode with diff-hl-flydiff-mode

--- lisp/init-vc.el
+++ lisp/init-vc.el
@@ -5,7 +5,6 @@
 (defun turn-on-diff-hl-flydiff-mode ()
   "Turn on diff-hl-flydiff-mode."
   (interactive)
-  (diff-hl-mode t)
   (diff-hl-flydiff-mode t))
 (add-hook 'prog-mode-hook 'turn-on-diff-hl-flydiff-mode)
 (add-hook 'vc-dir-mode-hook 'turn-on-diff-hl-flydiff-mode)
