580466f78ac6f3cf25a2ee63a01195c8f327f8e6
Choi Geonu <6566gun@gmail.com>
2017-02-18 18:55:01 +0900

    Add dir-local grep customizations

--- lisp/init-grep.el
+++ lisp/init-grep.el
@@ -8,5 +8,26 @@
 (when *is-a-mac*
   (setq-default locate-command "mdfind"))
 
+(defcustom local-grep-find-ignored-directories
+  nil
+  "Local grep-find-ignored-directories.")
+
+(defcustom local-grep-find-ignored-files
+  nil
+  "Local grep-find-ignored-files.")
+
+;; local grep-find ignored things
+(defun append-local-grep-find-ignored ()
+  "Append local-grep-find-ignored-* to grep-find-ignored-*."
+  (when local-grep-find-ignored-directories
+    (setq-local grep-find-ignored-directories
+                (append grep-find-ignored-directories
+                        local-grep-find-ignored-directories)))
+  (when local-grep-find-ignored-files
+    (setq-local grep-find-ignored-files
+                (append grep-find-ignored-files
+                        local-grep-find-ignored-files))))
+(add-hook 'hack-local-variables-hook #'append-local-grep-find-ignored)
+
 (provide 'init-grep)
 ;;; init-grep ends here
